f45636393fbc9cd9246f6b8fbdd6ee0fc11ddfd9
Eskathorn <aster2006777@outlook.com>
2025-06-02 00:11:28 +0700

    2

--- Model/DeliveredOrder.cs
+++ Model/DeliveredOrder.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    internal class DeliveredOrder:Order
+    internal class DeliveredOrder : Order
     {
-        public int CourierId {  get; set; }
+        public int CourierId { get; set; }
         public Adress DeliveryAdress { get; set; }
     }
 }
